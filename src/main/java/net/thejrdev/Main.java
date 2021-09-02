478054f28748843322a116decd48e853532610f0
dearaujo_882564 <stefan.dearaujo64@k12.leanderisd.org>
2021-09-02 15:27:41 -0500

    Added Taxicab Numbers

--- src/main/java/net/thejrdev/Main.java
+++ src/main/java/net/thejrdev/Main.java
@@ -5,11 +5,15 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(IntStream.range(1, Integer.parseInt(args[0])).filter(num -> new StringBuilder().append((num)).reverse().toString().equals((String.valueOf(num)))).count());
 
-        System.out.println(Arrays.toString(IntStream.range(1, Integer.parseInt(args[0])).filter(num -> (Math.sqrt(num) % 1 == 0 && Math.cbrt(num) % 1 == 0)).toArray()));
+        //System.out.println(IntStream.range(1, Integer.parseInt(args[0])).filter(num -> new StringBuilder().append((num)).reverse().toString().equals((String.valueOf(num)))).count());
+
+        //System.out.println(Arrays.toString(IntStream.range(1, Integer.parseInt(args[0])).filter(num -> (Math.sqrt(num) % 1 == 0 && Math.cbrt(num) % 1 == 0)).toArray()));
 
 
+        TaxiNumbers nums = new TaxiNumbers();
+
+        System.out.println(Arrays.toString(nums.taxiNumbers(100).toArray()));
 
 
         /*
--- src/main/java/net/thejrdev/TaxiNumbers.java
+++ src/main/java/net/thejrdev/TaxiNumbers.java
@@ -1,19 +1,31 @@
 package net.thejrdev;
 
+import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 public class TaxiNumbers {
 
-    public int[] taxiNumbers(int max){
+    public ArrayList<Long> taxiNumbers(int max){
 
-        IntStream stream = IntStream.range(1, max);
-        stream.map(n -> (int)Math.pow(n, 3));
-        int[] nums = stream.toArray();
-        int[] sums = new int[nums.length * nums.length];
-        for(int i = 0; i < nums.length; i++){
-            
+
+        ProgressBar progressBar = new ProgressBar();
+        int[] nums = IntStream.range(1, max).map(n -> (int)Math.pow(n, 3)).toArray();
+
+        ArrayList<Long> sums = new ArrayList<>();
+        ArrayList<Long> out = new ArrayList<>();
+        long sum;
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = i+1; j < nums.length; j++){
+                sum = (nums[i] + nums[j]);
+                if(sums.contains(sum)){
+                    out.add(sum);
+                }else{
+                    sums.add(sum);
+                }
+            }
+            System.out.print("\r" + progressBar.createBar(i, nums.length));
         }
-
+        return out;
     }
 
 }
